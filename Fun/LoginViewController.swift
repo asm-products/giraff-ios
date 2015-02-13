2e111fe174bae278646247068d9fa24bd575713b
Dave Newman <dave@snappyco.de>
2015-02-12 17:49:04 -0800

    Reuse launch image for login
    This was much harder than expected...

--- Fun/LoginViewController.swift
+++ Fun/LoginViewController.swift
@@ -2,15 +2,19 @@ import UIKit
 
 class LoginViewController: UIViewController, FBLoginViewDelegate {
     @IBOutlet weak var loginView: FBLoginView!
-
+    @IBOutlet weak var bgImage: UIImageView!
+    
+    var signedIn = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loginView.readPermissions = ["public_profile", "email"]
         loginView.delegate = self
-    }
-    
-    func loginViewShowingLoggedInUser(loginView: FBLoginView!) {
-        NSLog("Facebook already logged in")
+        
+        if let image = launchImage() {
+            bgImage.image = image
+            self.view.bringSubviewToFront(loginView)
+        }
     }
     
     func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
@@ -18,7 +22,36 @@ class LoginViewController: UIViewController, FBLoginViewDelegate {
         var email = user.objectForKey("email") as String
         FunSession.sharedSession.signIn(email) {
             NSLog("API signin successful")
-            self.performSegueWithIdentifier("loggedIn", sender: user)
+            if !self.signedIn {
+                self.performSegueWithIdentifier("loggedIn", sender: user)
+            }
+            self.signedIn = true
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        NSLog("signed in: \(self.signedIn)")
+    }
+    
+    func launchImage() -> UIImage? {
+        let fileName = launchImageFileName()
+        if let file = NSBundle.mainBundle().pathForResource(fileName, ofType: "png") {
+            return UIImage(contentsOfFile: file)!
+        }
+        return nil
+    }
+    
+    func launchImageFileName() -> String {
+        switch(UIScreen.mainScreen().bounds.size.height) {
+        case 568:
+            return "LaunchImage-700-568h@2x"
+        case 667:
+            return "LaunchImage-800-667h@2x"
+        case 736:
+            return "LaunchImage-800-Portrait-736h@3x"
+        default:
+            return "LaunchImage-700@2x"
         }
     }
 }
