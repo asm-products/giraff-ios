4035edf4b8ea29cd2b1058d93ccc5f505da0f524
Ben <hello@benshyong.com>
2015-02-19 12:31:56 -0800

    email address login proto

--- Fun/LoginViewController.swift
+++ Fun/LoginViewController.swift
@@ -3,7 +3,10 @@ import UIKit
 class LoginViewController: UIViewController, FBLoginViewDelegate {
     @IBOutlet weak var loginView: FBLoginView!
     @IBOutlet weak var bgImage: UIImageView!
-    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var emailLoginView: UIView!
+    @IBOutlet weak var emailLoginLabel: UILabel!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         loginView.readPermissions = ["public_profile", "email"]
@@ -13,8 +16,53 @@ class LoginViewController: UIViewController, FBLoginViewDelegate {
             bgImage.image = image
             self.view.bringSubviewToFront(loginView)
         }
+      
+      // keyboard
+      var center: NSNotificationCenter = NSNotificationCenter.defaultCenter()
+      center.addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+      center.addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
     }
-    
+
+    func keyboardWillShow(notification: NSNotification) {
+      
+      var info:NSDictionary = notification.userInfo!
+      var keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as NSValue).CGRectValue()
+      
+      var keyboardHeight:CGFloat = keyboardSize.height
+      
+      var animationDuration:CGFloat = info[UIKeyboardAnimationDurationUserInfoKey] as CGFloat
+      
+      UIView.animateWithDuration(0.25, delay: 0.25, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+        self.view.frame = CGRectMake(self.view.frame.origin.x, (self.view.frame.origin.y - keyboardHeight), self.view.bounds.width, self.view.bounds.height)
+        }, completion: nil)
+    }
+  
+    func keyboardWillHide(notification: NSNotification) {
+      var info:NSDictionary = notification.userInfo!
+      var keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as NSValue).CGRectValue()
+      
+      var keyboardHeight:CGFloat = keyboardSize.height
+      
+      var animationDuration:CGFloat = info[UIKeyboardAnimationDurationUserInfoKey] as CGFloat
+      
+      UIView.animateWithDuration(0.25, delay: 0.25, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+        self.view.frame = CGRectMake(self.view.frame.origin.x, (self.view.frame.origin.y + keyboardHeight), self.view.bounds.width, self.view.bounds.height)
+        }, completion: nil)
+    }
+  
+    override func viewWillDisappear(animated: Bool) {
+      super.viewWillDisappear(animated)
+      NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+      NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+  
+    func isValidEmail(testStr:String) -> Bool {
+      let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+      let range = testStr.rangeOfString(emailRegEx, options:.RegularExpressionSearch)
+      let result = range != nil ? true : false
+      return result
+    }
+
     func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
         NSLog("Facebook login successful")
         var email = user.objectForKey("email") as String
@@ -31,7 +79,22 @@ class LoginViewController: UIViewController, FBLoginViewDelegate {
             }
         }
     }
-    
+
+    @IBAction func loginButtonPressed(sender: AnyObject) {
+      if isValidEmail(emailField.text){
+        FunSession.sharedSession.signIn(emailField.text) {
+          NSLog("API signin successful")
+          dispatch_async(dispatch_get_main_queue()) {
+            self.performSegueWithIdentifier("loggedIn", sender: self)
+          }
+        }
+      } else {
+        var alert = UIAlertController(title: "Error", message: "Invalid Email", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Try Again", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+      }
+    }
+
     func loginViewShowingLoggedOutUser(loginView: FBLoginView!) {
         loginView.hidden = false
     }
